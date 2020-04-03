3e28ff1699550afb3619daa758ad35daa00ee5d6
Rémi Piotaix <remi@piotaix.fr>
2020-04-03 13:57:43 +0200

    Update game page

--- src/app/jeu/jeu.component.ts
+++ src/app/jeu/jeu.component.ts
@@ -7,33 +7,40 @@ import { SOCIAL_EDUKEY } from '../index/index.component'
     <eph-section type="top">
       <eph-section-title>
         <h2 translate>page.game.about.title</h2>
-        <h3 translate>page.game.the_game.title</h3>
       </eph-section-title>
       <eph-section-content>
         <div class="row">
           <div class="col">
+            <p [innerHTML]="'page.game.about.licence_desc' | translate"></p>
             <p [innerHTML]="'page.game.about.desc' | translate"></p>
-            <p translate>page.game.about.available</p>
           </div>
         </div>
 
         <div class="row">
-          <div class="col-12 col-md-10 mb-md-3">
+          <div class="col-sm-8 col-md-10 mb-md-3">
             <eph-yt-video videoId="qGeCoX1itRU" [social]="social"></eph-yt-video>
           </div>
         </div>
 
         <div class="row">
           <div class="col-sm-8">
-            <p translate>page.game.the_game.catchphrase</p>
-            <p translate>page.game.the_game.multiple_versions</p>
-            <p [innerHTML]="'page.game.the_game.created_for_the_needs_of_teachers' | translate"></p>
-            <p translate>page.game.the_game.will_you_take_the_challenge</p>
+            <p [innerHTML]="'page.game.about.for_teachers' | translate"></p>
+            <div class="row justify-content-center mb-3">
+              <div class="col mb-3 mb-md-0" align="center">
+                <eph-img src="assets/images/pages/jeu/players.png"></eph-img>
+              </div>
+              <div class="col mb-3 mb-md-0" align="center">
+                <eph-img src="assets/images/pages/jeu/age.png"></eph-img>
+              </div>
+              <div class="col mb-3 mb-md-0" align="center">
+                <eph-img src="assets/images/pages/jeu/time.png"></eph-img>
+              </div>
+            </div>
           </div>
           <div class="col-sm-4">
             <div style="display:block; text-align:center; padding-bottom:20px;">
               <a
-                href="https://docs.google.com/presentation/d/1DDVKwN7xkpdom1sa5Accj4L__awMs2Pc6LInrjp2Y8I/edit?usp=sharing"
+                href="assets/documents/2020_02_jeu_complet.pdf"
                 target="_blank"
                 rel="noreferrer"
                 class="btn btn-lg btn-secondary text-white font-weight-bold px-4 py-3"
@@ -68,28 +75,7 @@ import { SOCIAL_EDUKEY } from '../index/index.component'
       </eph-section-content>
     </eph-section>
 
-    <eph-section id="votre-version" type="mid">
-      <eph-section-title>
-        <h2 translate>page.game.create_your_version.title</h2>
-      </eph-section-title>
-      <eph-section-content>
-        <div class="row">
-          <div class="col">
-            <p [innerHTML]="'page.game.create_your_version.desc' | translate"></p>
-            <eph-button
-              href="https://docs.google.com/presentation/d/1bGQm1DqcPqc1H3-AySiSMIbnsxtRQnRtcUKAa2hgcs4"
-              target="_blank"
-              rel="noreferrer"
-            >
-              page.game.create_your_version.create_your_version
-            </eph-button>
-            <p [innerHTML]="'page.game.create_your_version.need_help' | translate"></p>
-          </div>
-        </div>
-      </eph-section-content>
-    </eph-section>
-
-    <eph-section type="bot">
+    <eph-section type="mid">
       <eph-section-title>
         <h2 translate>page.game.guide.title</h2>
       </eph-section-title>
@@ -99,6 +85,34 @@ import { SOCIAL_EDUKEY } from '../index/index.component'
             <p [innerHTML]="'page.game.guide.part_1' | translate"></p>
             <p [innerHTML]="'page.game.guide.part_2' | translate"></p>
             <p [innerHTML]="'page.game.guide.part_3' | translate"></p>
+            <!--            <eph-button-->
+            <!--              href="https://docs.google.com/presentation/d/1bGQm1DqcPqc1H3-AySiSMIbnsxtRQnRtcUKAa2hgcs4"-->
+            <!--              target="_blank"-->
+            <!--              rel="noreferrer"-->
+            <!--            >-->
+            <!--              page.game.guide.need_help-->
+            <!--            </eph-button>-->
+          </div>
+        </div>
+      </eph-section-content>
+    </eph-section>
+
+    <eph-section id="votre-version" type="bot">
+      <eph-section-title>
+        <h2 translate>page.game.create_your_version.title</h2>
+      </eph-section-title>
+      <eph-section-content>
+        <div class="row">
+          <div class="col">
+            <p [innerHTML]="'page.game.create_your_version.desc' | translate"></p>
+            <eph-button
+              href="https://docs.google.com/presentation/d/1uVQnyrAW0xyjGFuPl6v-EKY2ODSRZL8a-fvjCU2KMJQ/edit?usp=sharing"
+              target="_blank"
+              rel="noreferrer"
+            >
+              page.game.create_your_version.create_your_version
+            </eph-button>
+            <p [innerHTML]="'page.game.create_your_version.need_help' | translate"></p>
           </div>
         </div>
       </eph-section-content>
